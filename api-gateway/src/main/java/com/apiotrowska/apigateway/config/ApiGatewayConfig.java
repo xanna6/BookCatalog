8406e2f8bbedf64008c817f3d0e2017b57d5ec3e
xanna6 <apiotrow96@gmail.com>
2023-09-14 15:23:10 +0200

    added discovery server path mapping in api gateway config

--- api-gateway/src/main/java/com/apiotrowska/apigateway/config/ApiGatewayConfig.java
+++ api-gateway/src/main/java/com/apiotrowska/apigateway/config/ApiGatewayConfig.java
@@ -8,12 +8,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiGatewayConfig {
 
-//    @Bean
-//    public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("eureka", r -> r.path("/eureka/web")
-//                        .filters(f -> f.redirect(302, "/"))
-//                        .uri("http://localhost:8761"))
-//                .build();
-//    }
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("eureka", r -> r.path("/eureka/web")
+                        .filters(f -> f.redirect(302, "http://localhost:8761"))
+                        .uri("http://localhost:8761"))
+                .build();
+    }
 }
